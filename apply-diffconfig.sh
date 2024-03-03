2a49544deb51b81926fc68cc0c825ee33112e42d
come_ill_foo <rayveleaveme34@inbox.ru>
2024-03-03 23:42:38 +0000

    feat: tests diffconfigs; fix: bugs in script

--- apply-diffconfig.sh
+++ apply-diffconfig.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+# @brief No such file or directory
+ENOENT=2
+
 # @brief Invalid argument error code
 EINVAL=22
 
+CONFIG='scripts/config'
+VERBOSE=false
+PARAMS=()
+
 # TODO: document
-usage() {
-    cat << EOF
-Usage: $0 DIFF ORIG_CONFIG
-
-Applies diffconfig to kernel configuration
-
-Options:
-    -h, --help                  Prints this help message and exits
-    -c, --config scripts-config Path to scripts/config utility
-    -i, --in-place              Edit file in place
-
-EOF
-    exit $EINVAL
+exist_or_die() {
+    local path="$1"
+    if [ ! -f "${path}" ]; then
+        echo "Fatal: \"${path}\" not found"
+        exit $ENOENT
+    fi
+    return 0
 }
 
 # TODO: document
@@ -27,9 +28,31 @@ length() {
     return 0
 }
 
-INPLACE=false
-CONFIG='scripts/config'
-PARAMS=()
+# TODO: document
+log_run() {
+    local cmd="$*"
+    if $VERBOSE; then
+        echo "$*" >&2
+    fi
+    ${cmd}
+    return $?
+}
+
+# TODO: document
+usage() {
+    cat << EOF
+Usage: $0 diffconfig base-config
+
+Applies diffconfig to base kernel configuration
+
+Options:
+    -h, --help                  Prints this help message and exits
+    -c, --config scripts-config Path to scripts/config utility
+    -v, --verbose               Verbose mode
+
+EOF
+    exit $EINVAL
+}
 
 while [[ $# -gt 0 ]]; do
     case $1 in
@@ -37,12 +60,12 @@ while [[ $# -gt 0 ]]; do
             CONFIG="$2"
             shift 2
             ;;
-        -i|--in-place)
-            INPLACE=true
-            shift
-            ;;
         -h|--help)
             usage
+            ;;
+        -v|--verbose)
+            VERBOSE=true
+            shift
             ;;
         *)
             PARAMS+=("$1")
@@ -52,44 +75,59 @@ while [[ $# -gt 0 ]]; do
 done
 [ "$(length "${PARAMS[@]}")" -lt 2 ] && usage
 
-DIFF_CONFIG="${PARAMS[0]}"
-ORIG_CONFIG="${PARAMS[1]}"
+DIFFCONFIG="${PARAMS[0]}"
+BASECONFIG="${PARAMS[1]}"
+CONFIG_CMD="${CONFIG} --file ${BASECONFIG}"
 
-echo "Reading diff from ${DIFF_CONFIG}"
-echo "Reading config from ${ORIG_CONFIG}"
+exist_or_die "${DIFFCONFIG}"
+echo "Reading diff from ${DIFFCONFIG}"
+exist_or_die "${BASECONFIG}"
+echo "Reading config from ${BASECONFIG}"
 
 while read -r entry; do
     read -ra entry_parts <<<"$entry"
     config_option=${entry_parts[0]:1}
     config_value=''
+    config_cmd_args=()
+    should_parse_value=true
     case ${entry_parts[0]} in
+        -*)
+            config_cmd_args+=(--undefine "${config_option}")
+            should_parse_value=false
+            ;;
         +*)
             config_value=${entry_parts[1]}
-            ;;
-        -*)
-            ${CONFIG} --file "${ORIG_CONFIG}" --undefine "${config_option}"
-            continue
             ;;
         *)
             config_value="${entry_parts[3]}"
             ;;
     esac
     # parse option value
-    case $config_value in
-        y)
-            ${CONFIG} --file "${ORIG_CONFIG}" --enable "${config_option}"
-            ;;
-        m)
-            ${CONFIG} --file "${ORIG_CONFIG}" --module "${config_option}"
-            ;;
-        n)
-            ${CONFIG} --file "${ORIG_CONFIG}" --disable "${config_option}"
-            ;;
-        0x*)
-            ${CONFIG} --file "${ORIG_CONFIG}" --set-val "${config_option}" "${config_value}"
-            ;;
-        *)
-            ${CONFIG} --file "${ORIG_CONFIG}" --set-str "${config_option}" "${config_value}"
-            ;;
-    esac
-done < "${DIFF_CONFIG}"
+    if $should_parse_value; then
+        case $config_value in
+            y)
+                config_cmd_args+=(--enable "${config_option}")
+                ;;
+            m)
+                config_cmd_args+=(--module "${config_option}")
+                ;;
+            n)
+                config_cmd_args+=(--disable "${config_option}")
+                ;;
+            0x*)
+                config_cmd_args+=(--set-val "${config_option}" "${config_value}")
+                ;;
+            \"*)
+                config_cmd_args+=(--set-str "${config_option}" "${config_value}")
+                ;;
+            1*|2*|3*|4*|5*|6*|7*|8*|9*)
+                config_cmd_args+=(--set-val "${config_option}" "${config_value}")
+                ;;
+            *)
+                echo "Unknown value [${config_value}] for ${config_option}" >&2
+                exit $EINVAL
+                ;;
+        esac
+    fi
+    log_run "${CONFIG_CMD}" "${config_cmd_args[@]}"
+done < "${DIFFCONFIG}"
